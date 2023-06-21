ba38c8c6c69bb95d728ba40f9241b929c722a554
Lucas Bernardo <85906391+Bernardo-0@users.noreply.github.com>
2023-06-20 21:30:49 -0300

    Add files via upload

---
+++ ronaestrada/src/main/java/br/com/estacio/ronaestrada/Ronaestrada.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.estacio.ronaestrada;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ronaestrada {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ronaestrada.class, args);
+	}
+
+}
