6779113f369db7e552f908c128613de11ee0aadf
Lucas Bernardo <85906391+Bernardo-0@users.noreply.github.com>
2023-06-20 21:27:22 -0300

    Delete ronaestrada directory

--- ronaestrada/src/main/java/br/com/estacio/ronaestrada/repository/FuncionarioRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.estacio.ronaestrada.repository;
-
-import br.com.estacio.ronaestrada.model.Funcionario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer> {
-}
